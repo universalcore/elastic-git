3c4222c41f32b1adcce5b28187cf0b9f1d4f0114
Rizmari Versfeld <rizziepit@gmail.com>
2015-04-30 16:06:36 +0200

    test equivalence of old and new S.to_python method

--- elasticgit/tests/test_workspace.py
+++ elasticgit/tests/test_workspace.py
@@ -1,9 +1,12 @@
+from datetime import datetime
 import types
 import os
 
+from elasticutils import S as SBase
+
 from elasticgit.tests.base import ModelBaseTest, TestPerson, TestPage
 from elasticgit.search import ModelMappingType
-from elasticgit.workspace import Workspace
+from elasticgit.workspace import Workspace, S
 
 from git import Repo, GitCommandError
 
@@ -177,6 +180,27 @@ class TestEG(ModelBaseTest):
         workspace.refresh_index()
         model = result.to_object()
         self.assertNotEqual(model, person)
+
+    def test_search_results_to_python(self):
+        person_es_data = {
+            'age': 1,
+            'name': u'2020-01-01T06:00:00',
+            'uuid': u'foo',
+        }
+        person_es_data2 = person_es_data.copy()
+
+        S_eg = S()
+        S_original = SBase()
+        eg_person = S_eg.to_python(person_es_data)
+        original_person = S_original.to_python(person_es_data2)
+
+        # check that both methods operate in place
+        self.assertIs(person_es_data, eg_person)
+        self.assertIs(original_person, person_es_data2)
+        # check equality aside from datetime conversion
+        self.assertIsInstance(eg_person.pop('name'), basestring)
+        self.assertIsInstance(original_person.pop('name'), datetime)
+        self.assertEqual(eg_person, original_person)
 
     def test_access_elastic_search_data(self):
         workspace = self.workspace
