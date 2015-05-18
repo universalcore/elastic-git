b031761753f8f292b1ae8edbf32be04dca11fd7c
Simon de Haan <simon@praekeltfoundation.org>
2015-05-18 20:49:03 +0200

    tests for storage exists

--- elasticgit/remote_storage.py
+++ elasticgit/remote_storage.py
@@ -1,5 +1,16 @@
+import os
+from urlparse import urlparse
+
+import requests
+
 from zope.interface import implements
+
 from elasticgit.istorage import IStorageManager
+from elasticgit.utils import fqcn
+
+
+class RemoteStorageManagerException(Exception):
+    pass
 
 
 class RemoteStorageManager(object):
@@ -7,21 +18,47 @@ class RemoteStorageManager(object):
 
     def __init__(self, repo_url):
         self.repo_url = repo_url
+        parse_result = urlparse(self.repo_url)
+        self.scheme = parse_result.scheme
+        self.host = parse_result.netloc
+        self.port = parse_result.port
+        self.dir_name = os.path.dirname(parse_result.path)
+        basename = os.path.basename(parse_result.path)
+        self.repo_name, _, self.suffix = basename.partition('.')
+
+    def mk_request(self, *args, **kwargs):
+        return requests.request(*args, **kwargs)
+
+    def url(self, *parts):
+        path = [self.repo_name]
+        path.extend(parts)
+        return '%s://%s%s%s/%s.%s' % (
+            self.scheme,
+            self.host,
+            ':%s' % (self.port,) if self.port else '',
+            self.dir_name,
+            '/'.join(path),
+            self.suffix
+        )
 
     def write_config(self, section, data):
-        pass
+        raise RemoteStorageManagerException(
+            'Remote storage is read only.')
 
     def read_config(Self, section):
-        pass
+        raise RemoteStorageManagerException(
+            'Not implemented for remote storage.')
 
     def storage_exists(self):
-        pass
+        response = self.mk_request('GET', self.repo_url)
+        return response.status_code == requests.codes.ok
 
     def destroy_storage(self):
-        pass
+        raise RemoteStorageManagerException(
+            'Remote storage is read only.')
 
     def iterate(self, model_class):
-        pass
+        return self.url(fqcn(model_class))
 
     def get(self, model_class, uuid):
         pass
--- elasticgit/tests/test_remote_storage.py
+++ elasticgit/tests/test_remote_storage.py
@@ -1,13 +1,40 @@
 from elasticgit.tests.base import ModelBaseTest
 from elasticgit.istorage import IStorageManager
 from elasticgit.remote_storage import RemoteStorageManager
+from elasticgit.tests.base import TestPerson
+from elasticgit.utils import fqcn
+
+from requests.models import Response
+
+from mock import patch
 
 
 class TestRemoteStorage(ModelBaseTest):
 
-    def setUp(self):
-        self.rsm = RemoteStorageManager('http://www.example.org')
-
     def test_interface(self):
+        rsm = RemoteStorageManager('http://www.example.org/repos/foo.json')
         self.assertTrue(IStorageManager.implementedBy(RemoteStorageManager))
-        self.assertTrue(IStorageManager.providedBy(self.rsm))
+        self.assertTrue(IStorageManager.providedBy(rsm))
+
+    def test_url(self):
+        class_name = fqcn(TestPerson)
+        rsm = RemoteStorageManager('http://www.example.org/repos/foo.json')
+        self.assertEqual(
+            rsm.url(class_name),
+            'http://www.example.org/repos/foo/%s.json' % (class_name,))
+
+    def test_storage_exists(self):
+        rsm = RemoteStorageManager('http://www.example.org/repos/foo.json')
+        with patch.object(rsm, 'mk_request') as mock:
+            response = Response()
+            response.status_code = 200
+            mock.return_value = response
+            self.assertTrue(rsm.storage_exists())
+
+    def test_storage_doesnot_exist(self):
+        rsm = RemoteStorageManager('http://www.example.org/repos/foo.json')
+        with patch.object(rsm, 'mk_request') as mock:
+            response = Response()
+            response.status_code = 404
+            mock.return_value = response
+            self.assertFalse(rsm.storage_exists())
