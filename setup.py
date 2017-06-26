76298e3269f9a09b616f99f0c871b96ffb78c8e3
Jamie Hewland <jhewland@gmail.com>
2017-06-26 16:49:23 +0200

    Packaging cleanups like elsewhere

--- setup.py
+++ setup.py
@@ -6,29 +6,39 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst')) as f:
     README = f.read()
 
-with open(os.path.join(here, 'requirements.txt')) as f:
-    requires = filter(None, f.readlines())
-
 with open(os.path.join(here, 'VERSION')) as f:
     version = f.read().strip()
 
-setup(name='elastic-git',
-      version=version,
-      description='JSON Object storage backed by Git & Elastic Search',
-      long_description=README,
-      classifiers=[
-      "Programming Language :: Python",
-      ],
-      author='Praekelt Foundation',
-      author_email='unicore-team@praekeltconsulting.com',
-      url='http://github.com/universalcore/elastic-git',
-      license='BSD',
-      keywords='git elasticsearch json',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=requires,
-      tests_require=requires,
-      entry_points={
-          'console_scripts': ['eg-tools = elasticgit.tools:main'],
-      })
+install_requires = [
+    'avro==1.7.7',
+    'confmodel==0.2.0',
+    'elasticsearch==1.7.0',
+    'elasticutils==0.10.1',
+    'GitPython==1.0.2',
+    'Jinja2',
+    'requests',
+    'Unidecode==0.04.16',
+    'zope.interface',
+]
+
+setup(
+    name='elastic-git',
+    version=version,
+    description='JSON Object storage backed by Git & Elastic Search',
+    long_description=README,
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    author='Praekelt.org',
+    author_email='dev@praekelt.org',
+    url='http://github.com/universalcore/elastic-git',
+    license='BSD',
+    keywords='git elasticsearch json',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+      'console_scripts': ['eg-tools = elasticgit.tools:main'],
+    }
+)
